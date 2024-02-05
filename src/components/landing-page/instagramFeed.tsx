7b6d263f8a82bba1474ff4adeb3901ab94e01af2
romuloromulo <neoromulo@gmail.comgit config --global user.email neoromulo@gmail.comclgit config --global user.name Rmulo>
2024-02-05 20:59:37 -0300

    refazendo instagramFeed

--- src/components/landing-page/instagramFeed.tsx
+++ src/components/landing-page/instagramFeed.tsx
@@ -1,47 +1,15 @@
-// InstagramFeed.tsx
 import React, { useEffect, useState } from "react";
-import axios from "axios";
-import Image from "next/image";
-import { Button } from "../ui/button";
-import {
-  Tooltip,
-  TooltipContent,
-  TooltipProvider,
-  TooltipTrigger,
-} from "@/components/ui/tooltip";
-import Link from "next/link";
-import Loading from "../global/loader";
-
-// Defina um tipo para os dados da resposta da API
-interface InstagramPhoto {
-  id: string;
-  caption: string;
-  media_type: string;
-  media_url: string;
-  thumbnail_url: string;
-  permalink: string;
-  timestamp: string;
-}
+import { InstagramEmbed } from "react-social-media-embed";
 
 const InstagramFeed = () => {
-  const [photos, setPhotos] = useState<InstagramPhoto[]>([]);
-  const [showAllPhotos, setShowAllPhotos] = useState(false);
-  const [loading, setLoading] = useState(true);
-
-  // useEffect(() => {
-  //   const fetchData = async () => {
-  //     const response = await fetch("/api/instagramfeedapi");
-  //     const data = await response.json();
-  //     setPhotos(data);
-  //     setLoading(false);
-  //     // console.error("Error fetching Instagram photos:", error);
-  //     setLoading(false);
-  //   };
-
-  //   fetchData();
-  // }, []);
-
-  const displayedPhotos = showAllPhotos ? photos : photos.slice(0, 9);
+  const instagramLinks = [
+    "https://www.instagram.com/p/CzoDEcXOOpT/",
+    "https://www.instagram.com/p/C25Oo61pIBY/",
+    "https://www.instagram.com/p/CzBt05xOv_1/",
+    "https://www.instagram.com/p/CygcZHgJS4_/",
+    "https://www.instagram.com/p/C28LgIWp0lJ/",
+    "https://www.instagram.com/p/CrMgf5BpQci/",
+  ];
 
   return (
     <section
@@ -53,53 +21,15 @@ const InstagramFeed = () => {
           CONHEÇA UM POUCO MAIS DA NOSSA ACADEMIA
         </h2>
       </div>
-      {loading ? (
-        <Loading />
-      ) : (
-        <div className="flex justify-center">
-          <div className="grid grid-cols-1  md:grid-cols-2 lg:grid-cols-3 gap-4">
-            {displayedPhotos.map((photo) => {
-              if (
-                photo.media_url.includes("mp4") ||
-                photo.media_url.includes("m69")
-              )
-                return;
-              return (
-                <TooltipProvider key={photo.id}>
-                  <Tooltip>
-                    <TooltipTrigger>
-                      <Link href={photo.permalink}>
-                        <div className="md:w-[20rem] md:h-[20rem] w-[17rem] h-[17rem] rounded-lg  overflow-hidden">
-                          <Image
-                            src={photo.media_url}
-                            alt={photo.caption}
-                            width={500}
-                            height={500}
-                          />
-                        </div>
-                      </Link>
-                    </TooltipTrigger>
-                    <TooltipContent className="max-w-[15rem] max-h-[7rem]">
-                      {photo.caption}
-                    </TooltipContent>
-                  </Tooltip>
-                </TooltipProvider>
-              );
-            })}
-          </div>
+
+      <div className="flex justify-center">
+        <div className="grid grid-cols-1  md:grid-cols-2 lg:grid-cols-3 gap-4 items-center justify-center">
+          {instagramLinks.map((links) => (
+            <div className="shadow-lg shadow-gray-900">
+              <InstagramEmbed url={links} width={328} />
+            </div>
+          ))}
         </div>
-      )}
-      <div className="w-full justify-center flex mt-8">
-        {!showAllPhotos && (
-          <Button onClick={() => setShowAllPhotos(true)} className="mx-auto">
-            Mostrar Mais
-          </Button>
-        )}
-        {showAllPhotos && (
-          <Button onClick={() => setShowAllPhotos(false)} className="mx-auto">
-            Mostrar Menos
-          </Button>
-        )}
       </div>
     </section>
   );
