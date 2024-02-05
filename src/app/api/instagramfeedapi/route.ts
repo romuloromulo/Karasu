94383c045018984608d5a50e6efd750c9b77fa36
romuloromulo <neoromulo@gmail.comgit config --global user.email neoromulo@gmail.comclgit config --global user.name Rmulo>
2024-02-05 15:59:46 -0300

    testand

--- src/app/api/instagramfeedapi/route.ts
+++ src/app/api/instagramfeedapi/route.ts
@@ -3,11 +3,21 @@ import axios from "axios";
 import { NextResponse } from "next/server";
 
 export async function GET() {
-  const response = await axios.get(
-    `https://graph.instagram.com/me/media?fields=id,caption,media_url,timestamp,permalink&access_token=${process.env.TOKEN_ACESSO}`
-  );
+  try {
+    const response = await axios.get(
+      `https://graph.instagram.com/me/media?fields=id,caption,media_url,media_type,timestamp,permalink&access_token=${process.env.TOKEN_ACESSO}`
+    );
 
-  const photos = response.data.data;
+    console.log("Resposta da API do Instagram:", response.data);
 
-  return NextResponse.json(photos);
+    const photos = response.data.data;
+
+    return NextResponse.json(photos);
+  } catch (error) {
+    console.error("Erro na solicitação à API do Instagram:", error);
+    return NextResponse.json(
+      { error: "Erro na solicitação à API do Instagram" },
+      { status: 500 }
+    );
+  }
 }
