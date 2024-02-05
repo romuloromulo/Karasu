8d35b8d5ff4124bf16d645c33c8acc23d2f2d361
romuloromulo <neoromulo@gmail.comgit config --global user.email neoromulo@gmail.comclgit config --global user.name Rmulo>
2024-02-05 15:02:29 -0300

    testando api

--- src/app/api/instagramfeedapi/route.ts
+++ src/app/api/instagramfeedapi/route.ts
@@ -3,7 +3,6 @@ import axios from "axios";
 import { NextRequest } from "next/server";
 import { NextResponse } from "next/server";
 
-export const runtime = "edge";
 export async function GET(req: NextRequest) {
   const response = await axios.get(
     `https://graph.instagram.com/me/media?fields=id,caption,media_url,timestamp,permalink&access_token=${process.env.TOKEN_ACESSO}`
